72b605e072413666a8d9013394251081bd779f70
MashobTechie <sheriffdeenmakinde@gmail.com>
2024-11-22 08:40:40 +0000

    nothing much

--- app/posts/[postID]/page.js
+++ app/posts/[postID]/page.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 // Having square bracket in the folder name gives this a dynamic url. You can access unique pages with this.
 // The route wil be localhost:3000/:postID
+// Stooped here
 
 const page = () => {
   return (
